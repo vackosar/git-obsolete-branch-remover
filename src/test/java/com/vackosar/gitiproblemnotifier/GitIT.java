e03a649ec0589a1f1b0c3301ed04b0d93ef80080
vakosar <vackosar>
2015-10-24 22:31:37 +0200

    #1 Implement daemon partially.

--- src/test/java/com/vackosar/gitiproblemnotifier/GitIT.java
+++ src/test/java/com/vackosar/gitiproblemnotifier/GitIT.java
@@ -4,9 +4,20 @@ import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.ListBranchCommand;
 import org.eclipse.jgit.api.ResetCommand;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.errors.RepositoryNotFoundException;
+import org.eclipse.jgit.internal.storage.dfs.DfsRepositoryDescription;
+import org.eclipse.jgit.internal.storage.dfs.InMemoryRepository;
 import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.lib.StoredConfig;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.transport.Daemon;
+import org.eclipse.jgit.transport.DaemonClient;
+import org.eclipse.jgit.transport.ServiceMayNotContinueException;
+import org.eclipse.jgit.transport.resolver.RepositoryResolver;
+import org.eclipse.jgit.transport.resolver.ServiceNotAuthorizedException;
+import org.eclipse.jgit.transport.resolver.ServiceNotEnabledException;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -15,12 +26,8 @@ import org.junit.Assert;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.util.Arrays;
-import java.util.Date;
-import java.util.List;
+import java.net.InetSocketAddress;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class GitIT {
@@ -70,6 +77,16 @@ public class GitIT {
         Assert.assertArrayEquals(new String[]{REPODIRNAME, FILE.getName()}, DIR.list());
     }
 
+    @Test
+    public void push() throws GitAPIException, IOException, InterruptedException {
+        final Git git = commitFile();
+        startRemoteDaemon(git);
+        git.push().call();
+        FILE.delete();
+        git.pull().call();
+        Thread.sleep(60000);
+    }
+
     private Git commitFile() throws GitAPIException, IOException {
         final Git git = initialize();
         FILE.createNewFile();
@@ -105,6 +122,40 @@ public class GitIT {
         Thread.sleep(1000);
         Assert.assertTrue(getSecondsFromEpoch() - commit.getCommitTime() > 0);
     }
+
+    private void startRemoteDaemon(Git git) throws GitAPIException, IOException {
+        Daemon server = new Daemon(new InetSocketAddress(9418));
+        boolean uploadsEnabled = true;
+        server.getService("git-receive-pack").setEnabled(uploadsEnabled);
+        server.setRepositoryResolver(new RepositoryResolverImplementation());
+        server.start();
+        StoredConfig config = git.getRepository().getConfig();
+        config.setString("remote", "origin", "url", "git://localhost/repo.git");
+        config.setString("remote", "origin" ,"fetch", "+refs/heads/*:refs/remotes/origin/*");
+        config.setString("branch", "master", "remote", "origin");
+        config.setString("branch", "master", "merge", "refs/heads/master");
+        System.out.println(config);
+        config.save();
+    }
+
+    private static final class RepositoryResolverImplementation implements
+            RepositoryResolver<DaemonClient> {
+        @Override
+        public Repository open(DaemonClient client, String name)
+                throws RepositoryNotFoundException,
+                ServiceNotAuthorizedException, ServiceNotEnabledException,
+                ServiceMayNotContinueException {
+            InMemoryRepository repo = repositories.get(name);
+            if (repo == null) {
+                repo = new InMemoryRepository(
+                        new DfsRepositoryDescription(name));
+                repositories.put(name, repo);
+            }
+            return repo;
+        }
+    }
+
+    private static Map<String, InMemoryRepository> repositories = new HashMap<String, InMemoryRepository>();
 
     private int getSecondsFromEpoch() {
         return (int) (new Date().getTime() / 1000);
