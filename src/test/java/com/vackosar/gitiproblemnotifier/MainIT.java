f4054db5c2dbdfb1a24f229e2092683eb4d3c883
vackosar <vackosar@github.com>
2016-02-10 22:06:18 +0100

    Prepare ssh remote repo it.

---
+++ src/test/java/com/vackosar/gitiproblemnotifier/MainIT.java
@@ -0,0 +1,16 @@
+package com.vackosar.gitiproblemnotifier;
+
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.junit.Test;
+
+public class MainIT {
+
+    @Test
+    public void fetch() throws GitAPIException {
+        try (
+                LocalRepoMock localRepoMock = new LocalRepoMock(RemoteSshRepo.URL);
+        ){
+
+        }
+    }
+}
